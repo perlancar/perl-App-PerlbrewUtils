7f39dd794740d6e425e847e6c18f761fe2a1b1fa
perlancar (@pc-office) <perlancar@gmail.com>
2016-03-17 19:49:41 +0700

    Add common filtering options: --include, --exclude, --include-version, --exclude-version, --min-version, --max-version, --xmin-version, --xmax-version

--- lib/App/PerlbrewUtils.pm
+++ lib/App/PerlbrewUtils.pm
@@ -1,8 +1,167 @@
 package App::PerlbrewUtils;
 
 # DATE
-# DIST
 # VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %common_args = (
+    include => {
+        summary => 'Include perl(s) by name',
+        schema => ['array*', of=>'str*'],
+        tags => ['category:filtering'],
+    },
+    exclude => {
+        summary => 'Exclude perl(s) by name',
+        schema => ['array*', of=>'str*'],
+        tags => ['category:filtering'],
+    },
+    include_version => {
+        summary => 'Include perl(s) by version',
+        description => <<'_',
+
+You can specify e.g. `5.22` to include all 5.22.* version.
+
+_
+        schema => ['array*', of=>'str*'],
+        tags => ['category:filtering'],
+    },
+    exclude_version => {
+        summary => 'Exclude perl(s) by version',
+        description => <<'_',
+
+You can specify e.g. `5.22` to exclude all 5.22.* version.
+
+_
+        schema => ['array*', of=>'str*'],
+        tags => ['category:filtering'],
+    },
+    min_version => {
+        summary => 'Minimum perl version to include',
+        schema => ['str*'],
+        tags => ['category:filtering'],
+    },
+    max_version => {
+        summary => 'Maximum perl version to include',
+        schema => ['str*'],
+        tags => ['category:filtering'],
+    },
+    xmin_version => {
+        summary => 'Only include perl version greater than this',
+        schema => ['str*'],
+        tags => ['category:filtering'],
+    },
+    xmax_version => {
+        summary => 'Only include perl version lesser than this',
+        schema => ['str*'],
+        tags => ['category:filtering'],
+    },
+);
+
+sub _version_eq {
+    my ($v, $spec) = @_;
+
+    if ($spec =~ s/^v?(\d+)$/v$1/) {
+        $v =~ s/^v?(\d+).+/v$1/;
+    } elsif ($spec =~ s/^v?(\d+\.\d+)$/v$1/) {
+        $v =~ s/^v?(\d+\.\d+).+/v$1/;
+    }
+    version->parse($v) == version->parse($spec);
+}
+
+sub _version_gt {
+    my ($v, $spec) = @_;
+
+    if ($spec =~ s/^v?(\d+)$/v$1/) {
+        $v =~ s/^v?(\d+).+/v$1/;
+    } elsif ($spec =~ s/^v?(\d+\.\d+)$/v$1/) {
+        $v =~ s/^v?(\d+\.\d+).+/v$1/;
+    }
+    my $res = version->parse($v) > version->parse($spec);
+    #say "D:comparing version $v vs $spec: $res";
+    $res;
+}
+
+sub _version_lt {
+    my ($v, $spec) = @_;
+
+    if ($spec =~ s/^v?(\d+)$/vv$1/) {
+        $v =~ s/^v?(\d+).+/v$1/;
+    } elsif ($spec =~ s/^v?(\d+\.\d+)$/v$1/) {
+        $v =~ s/^v?(\d+\.\d+).+/v$1/;
+    }
+    my $res = version->parse($v) < version->parse($spec);
+    #say "D:comparing version $v vs $spec: $res";
+    $res;
+}
+
+sub _filter_perl {
+    my ($perl, $args) = @_;
+
+    #say "D:filtering perl $perl->{version} ...";
+
+  FILTER_INCLUDE:
+    {
+        last unless $args->{include} && @{ $args->{include} };
+        for (@{ $args->{include} }) {
+            last FILTER_INCLUDE if $perl->{name} eq $_;
+        }
+        return 0;
+    }
+
+  FILTER_EXCLUDE:
+    {
+        last unless $args->{exclude} && @{ $args->{exclude} };
+        for (@{ $args->{exclude} }) {
+            return 0 if $perl->{name} eq $_;
+        }
+    }
+
+  FILTER_INCLUDE_VERSION:
+    {
+        last unless $args->{include_version} && @{ $args->{include_version} };
+        for (@{ $args->{include_version} }) {
+            last FILTER_INCLUDE_VERSION if _version_included($perl->{version}, $_);
+        }
+        return 0;
+    }
+
+  FILTER_EXCLUDE_VERSION:
+    {
+        last unless $args->{exclude_version} && @{ $args->{exclude_version} };
+        for (@{ $args->{exclude_version} }) {
+            return 0 if _version_included($perl->{version}, $_);
+        }
+    }
+
+  FILTER_MIN_VERSION:
+    {
+        last unless $args->{min_version};
+        return 0 if _version_lt($perl->{version}, $args->{min_version});
+    }
+
+  FILTER_MAX_VERSION:
+    {
+        last unless $args->{max_version};
+        return 0 if _version_gt($perl->{version}, $args->{max_version});
+    }
+
+  FILTER_XMIN_VERSION:
+    {
+        last unless $args->{xmin_version};
+        return 0 if !_version_gt($perl->{version}, $args->{xmin_version});
+    }
+
+  FILTER_XMAX_VERSION:
+    {
+        last unless $args->{xmax_version};
+        return 0 if !_version_lt($perl->{version}, $args->{xmax_version});
+    }
+
+    1;
+}
 
 1;
 # ABSTRACT: More utilities for perlbrew
